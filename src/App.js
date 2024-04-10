442ab5bd583cae59b36cb56c6f982eb0bb67e13a
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-10 06:48:05 -0300

    embla_carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import {EmblaCarousel} from './components/EmblaCarousel';
 
 function App() {
   return (
     <div className="App">
+      <EmblaCarousel /> 
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/EmblaCarousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+
+export function EmblaCarousel() {
+  const [emblaRef] = useEmblaCarousel()
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide">Slide 1</div>
+        <img src="https://motos.honda.com.ar/uploads/home/slides/70familia-xr.jpg" alt="imagen1" />
+        <div className="embla__slide">Slide 2</div>
+        <img src="https://motos.honda.com.ar/uploads/home/slides/73slide-motos-tornado-3-anos-garantia.jpg" alt="imagen2" />
+        <div className="embla__slide">Slide 3</div>
+        <img src="https://motos.honda.com.ar/uploads/home/slides/56slides-motos-2023-wave.jpg" alt="imagen3" />
+      </div>
+    </div>
+  )
+}
