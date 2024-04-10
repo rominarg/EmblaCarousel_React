88458d9a2d4edd5c58195c8579cf82566cc13d88
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-10 06:54:23 -0300

    embla_carousel

--- src/components/EmblaCarousel.jsx
+++ src/components/EmblaCarousel.jsx
@@ -7,12 +7,14 @@ export function EmblaCarousel() {
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <img src="https://motos.honda.com.ar/uploads/home/slides/70familia-xr.jpg" alt="imagen1" />
-        <div className="embla__slide">Slide 2</div>
-        <img src="https://motos.honda.com.ar/uploads/home/slides/73slide-motos-tornado-3-anos-garantia.jpg" alt="imagen2" />
-        <div className="embla__slide">Slide 3</div>
-        <img src="https://motos.honda.com.ar/uploads/home/slides/56slides-motos-2023-wave.jpg" alt="imagen3" />
+        <div className="embla__slide"><img src="https://motos.honda.com.ar/uploads/home/slides/70familia-xr.jpg" alt="imagen1" />
+        </div>
+        <div className="embla__slide">
+            <img src="https://motos.honda.com.ar/uploads/home/slides/73slide-motos-tornado-3-anos-garantia.jpg" alt="imagen2" />
+            </div>
+        <div className="embla__slide">
+            <img src="https://motos.honda.com.ar/uploads/home/slides/56slides-motos-2023-wave.jpg" alt="imagen3" />
+        </div>
       </div>
     </div>
   )
